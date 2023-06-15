f7e10b7b7bd192179cd878c05aaa4bfe77ac37d8
unknown <ezegeek@gmail.com>
2023-06-15 12:42:25 -0300

    Created addTask function

---
+++ src/addTask.js
@@ -0,0 +1,41 @@
+const { v4 } = require('uuid');
+const AWS = require('aws-sdk');
+
+const addTask = async (event) => {
+
+    const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+    const { title, description } = JSON.parse(event.body);
+    const createdAt = new Date().toUTCString();
+    const id = v4();
+
+    const newTask = {
+        id,
+        title,
+        description,
+        createdAt,
+    };
+
+    try {
+        await dynamodb.put({
+            TableName: 'TaskTable',
+            Item: newTask
+        }).promise();
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(newTask)
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            error,
+            body: JSON.stringify(newTask)
+        };
+    }
+
+}
+
+module.exports = {
+    addTask
+}
